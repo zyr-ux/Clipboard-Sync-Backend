433c052e2ba7504173365577c29eda1d7e8accb2
zyr-ux <siddharthasxi2003@gmail.com>
2025-07-31 18:38:17 +0530

    fixed a import

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from auth import SECRET_KEY, ALGORITHM
 from fastapi.security import OAuth2PasswordBearer
 from fastapi_limiter import FastAPILimiter
 from fastapi_limiter.depends import RateLimiter
-import aioredis
+from redis.asyncio import Redis
 from connection_manager import ConnectionManager
 from utils import cleanup_expired_refresh_tokens
 
