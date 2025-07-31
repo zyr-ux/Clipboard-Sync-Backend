4292b07b732258188a9c2fbc72a7e6b348664ead
zyr-ux <siddharthasxi2003@gmail.com>
2025-07-31 17:48:58 +0530

    fixed a bug

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from sqlalchemy.orm import Session
-from models import BlacklistedToken
+from models import BlacklistedToken, RefreshToken
 
 def cleanup_expired_blacklisted_tokens(db: Session):
     db.query(BlacklistedToken).filter(BlacklistedToken.expiry < datetime.utcnow()).delete()
